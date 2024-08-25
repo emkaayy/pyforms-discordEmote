fb452d62b50d5aa45a64aa608be67014fd290e5c
Alex Englert <alex.englert.22@cnu.edu>
2024-08-25 19:40:31 -0400

    not initial (alex's house)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
 import PIL
 import PIL.Image
 import PIL.ImageTk
@@ -8,6 +9,18 @@ import PIL.ImageTk
 start_coords = []
 squarex = 0
 squarey = 0
+displayImage = ""
+filename = ""
+mydict = {"imagetk": None}
+
+def imageSelect(r):
+    global filename
+    filename = filedialog.askopenfile()
+    filename = filename.name
+    image = PIL.Image.open(filename)
+    r["imagetk"] = PIL.ImageTk.PhotoImage(image)
+    canvas.itemconfig(displayImage, image=r['imagetk'])
+    return filename
 
 def motion(e):
     global squarex
@@ -21,24 +34,40 @@ def motion(e):
 root = Tk()
 root.title = "Discord Emoji Cropper and Exporter"
 
+menu = Menu(root)
+
+def mywrapper():
+    imageSelect(mydict)
+
+filemenu = Menu(menu, tearoff=0)
+filemenu.add_command(label="open", command=mywrapper)
+filemenu.add_command(label="exit", command=root.quit)
+
+menu.add_cascade(label="file", menu=filemenu)
+
 frame = ttk.Frame(root, padding="1 1 1 1")
 
-# create canvas
+# change image to be selected image
+
 # adjust canvas from image res
+
+# create canvas
 canvas = Canvas(root, width=496, height=480)
 canvas.pack()
 
 canvas.bind('<B1-Motion>', motion)
 
 # display image
-# change image to be selected image
-image = PIL.Image.open("Bea_hajiraia2.png")
+filename = imageSelect(r=mydict)
+print("filename: " + filename)
+image = PIL.Image.open(filename)
 imagetk = PIL.ImageTk.PhotoImage(image)
-canvas.create_image(20, 20, anchor=NW, image=imagetk)
+displayImage = canvas.create_image(20, 20, anchor=NW, image=imagetk)
 
 # draw selection square
 square = canvas.create_rectangle(squarex, squarey, 128, 128, fill="black", outline="gray", width=3, stipple='gray25')
 
+root.config(menu=menu)
 root.mainloop()
 
 # export image
